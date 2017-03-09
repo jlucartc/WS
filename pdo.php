1b3cb9aac99a8d020faa4546f069193fffe42a01
Jlucartc <jlucartc@gmail.com>
2017-03-08 22:47:35 -0300

    commit

--- pdo.php
+++ pdo.php
@@ -1,10 +1,10 @@
 <?php
 
 define("DB","mysql");
-define("DB_HOST","fdb16.biz.nf:3306");
-define("DB_NAME","2309980_ws");
-define("DB_USER","2309980_ws");
-define("DB_PASS","1234abcde");
+define("DB_HOST","localhost");
+define("DB_NAME","ws");
+define("DB_USER","root");
+define("DB_PASS","");
 
 $pdo = new PDO(DB.":host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PASS);
 
